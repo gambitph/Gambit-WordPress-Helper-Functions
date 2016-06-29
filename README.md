15317366419cddfc968739e547e0b9f6222de384
Benjamin Intal <bfintal@gmail.com>
2016-06-29 13:23:16 +0800

    Added gambit_hex_to_rgb function

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Use it:
 | `gambit_get_all_post_types()` | `array` | Gets all the post types currently registered. Returns an associative array of all post type slugs and post type names. |
 | `gambit_get_current_url()` | `string` | Gets the current URL. |
 | `gambit_abbreviate_number($value)` | `string` | Abbreviates a number with a unit. E.g. Converts 1100 to 1.1K |
+| `gambit_hex_to_rgb($hex)` | `array` | Converts a hex color `#ffffff` or `#fff` to rgb `array( 255, 255, 255 )` |
--- gambit-helpers.php
+++ gambit-helpers.php
@@ -138,3 +138,30 @@ if ( ! function_exists( 'gambit_abbreviate_number' ) ) {
 		return $value;
 	}
 }
+
+
+if ( ! function_exists( 'gambit_hex_to_rgb' ) ) {
+
+	/**
+	 * Converts a hex color to rgb values.
+	 *
+	 * @param string $hex The hexadecimal color valuess.
+	 *
+	 * @return array The individual r, g, b colors
+	 */
+	function gambit_hex_to_rgb( $hex ) {
+		$hex = str_replace( '#', '', $hex );
+
+		if ( 3 !== strlen( $hex ) ) {
+			$r = hexdec( substr( $hex, 0, 1 ).substr( $hex, 0, 1 ) );
+			$g = hexdec( substr( $hex, 1, 1 ).substr( $hex, 1, 1 ) );
+			$b = hexdec( substr( $hex, 2, 1 ).substr( $hex, 2, 1 ) );
+		} else {
+			$r = hexdec( substr( $hex, 0, 2 ) );
+			$g = hexdec( substr( $hex, 2, 2 ) );
+			$b = hexdec( substr( $hex, 4, 2 ) );
+		}
+
+		return array( $r, $g, $b );
+	}
+}
